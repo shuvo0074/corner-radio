7ba2b98f0c702299ae45b0f3e0c1c0b9c191d090
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2021-06-16 20:03:06 +0600

    add list items

--- src/components/ListItem/listItem.js
+++ src/components/ListItem/listItem.js
@@ -3,8 +3,12 @@ import "./listItem.css";
 function ListItem({item}) {
   return (
     <div className="item-Container">
-      <h1>{item.title}</h1>
-      <h1>{item.userCount}</h1>
+      <p
+        className="fs-25 fw-400"
+      >{item.title}</p>
+      <p
+        className="fs-22 fw-bold"
+      >{item.userCount}</p>
     </div>
   );
 }
