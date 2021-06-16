810039e482f608d0d796435d67b833eaf5b25f73
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2021-06-16 22:51:48 +0600

    move setting data to useEffect

--- src/components/RadioList/radioList.js
+++ src/components/RadioList/radioList.js
@@ -1,11 +1,17 @@
 import ListItem from "../ListItem/listItem";
 import "./radioList.css";
 import { radioList } from "../../const/radioList";
+import { useEffect, useState } from "react";
 
 function RadioList({ currentlyPlaying, setcurrentlyPlaying }) {
+  const [ items, setItems] = useState([])
+  useEffect(()=>{
+    // do API request here
+    setItems(radioList)
+  },[])
   return (
     <div className="list-Container">
-      {radioList.map((item, index) => (
+      {items.map((item, index) => (
         <ListItem key={index} onClick={()=>setcurrentlyPlaying(item)} isExpanded={index == currentlyPlaying.id} item={item} />
       ))}
     </div>
