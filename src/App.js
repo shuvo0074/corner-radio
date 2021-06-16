eca65f556d1406d6b00705f8f711b890594d4f35
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2021-06-16 11:36:47 +0600

    add list items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import ListHeader from './components/ListHeader/listHeader';
+import "./App.css";
+import ListHeader from "./components/ListHeader/listHeader";
+import ListItem from "./components/ListItem/listItem";
+import { radioList } from "./const/radioList";
 
 function App() {
   return (
     <div className="App">
       <div className="Radio-container">
-        <ListHeader/>
+        <ListHeader />
+        {radioList.map((item,index)=>
+        <ListItem key={index} item={item} />
+        )}
       </div>
     </div>
   );
---
+++ src/const/radioList.js
@@ -0,0 +1,42 @@
+export const radioList =[
+    {
+        title: "Putin FM",
+        userCount: 666,
+        url: "http://s2.cdnradio.ru/ru-mp3-128"
+    },
+    {
+        title: "Ballads FM",
+        userCount: 1012,
+        url: "https://streamingv2.shoutcast.com/JamendoLounge"
+    },
+    {
+        title: "Dribble FM",
+        userCount: 994,
+        url: "http://s3.voscast.com:9836/stream"
+    },
+    {
+        title: "Doge FM",
+        userCount: 994,
+        url: "http://ice10.fluidstream.net:8080/calciofvg.mp3"
+    },
+    {
+        title: "Maximum FM",
+        userCount: 994,
+        url: "http://cdn01.radio.cloud/bcn-client27-glx"
+    },
+    {
+        title: "Rock FM",
+        userCount: 994,
+        url: "http://stream.diazol.hu:35200/live"
+    },
+    {
+        title: "Top FM",
+        userCount: 994,
+        url: "http://s3.voscast.com:9836/stream"
+    },
+    {
+        title: "Sunset FM",
+        userCount: 994,
+        url: "http://s2.cdnradio.ru/ru-mp3-128"
+    },
+]
